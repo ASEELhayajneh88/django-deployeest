23bf11547860e987df46aa6f093e6e43b1cda8f8
aseel <aseelhayajneh88@gmail.com>
2024-02-20 18:18:02 +0300

    new

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cp^t=c7&lbprztozgv=@@hab!^%^4dm75*xs$4882va7zvir(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,14 +76,25 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {   'default':
-    {      
-     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-     'NAME': 'dataa',    
-     'USER': 'aseel1',      
-     'PASSWORD': '581999',    
-     'HOST': '127.0.0.1',     
-     'PORT': '5432',   }}
+
+DATABASES={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+    
+
+    
+
+# DATABASES = {   'default':
+#     {      
+#      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#      'NAME': 'dataa',    
+#      'USER': 'aseel1',      
+#      'PASSWORD': '581999',    
+#      'HOST': '127.0.0.1',     
+#      'PORT': '5432',   }}
 
 
 
